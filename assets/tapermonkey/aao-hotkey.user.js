6c87edefcfcc34a7dfb7cc6717b6d31fa09771e0
onekintaro <kintaro221@gmail.com>
2024-05-12 21:34:09 +0200

    Aktualisiere AAO Hotkey Zuweisung auf Version 1.0

--- assets/tapermonkey/aao-hotkey.user.js
+++ assets/tapermonkey/aao-hotkey.user.js
@@ -1,12 +1,12 @@
 // ==UserScript==
 // @name         Erweiterte AAO Hotkey Zuweisung
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      1.0
 // @description  Erlaube die Zuweisung von erweiterten Tastenkombinationen für AAO Hotkeys im Leitstellenspiel.
 // @author       Raphi
 // @match        https://leitstellenspiel.de/*
 // @match        https://www.leitstellenspiel.de/*
 // @match        https://polizei.leitstellenspiel.de/*
 // @grant        none
-// @require      https://cdn.jsdelivr.net/gh/onekintaro/lss-hotkeys-plus/dist/bundle.js
+// @require      https://raw.githubusercontent.com/onekintaro/lss-hotkeys-plus/main/dist/bundle.js
 // ==/UserScript==
